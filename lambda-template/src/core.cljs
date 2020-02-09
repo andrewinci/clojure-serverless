520912f727cbae6f544f1ae3c9fea29c0a2fc492
Andrea Vinci <andrewinci@gmail.com>
2020-02-09 11:37:44 +0000

    Bundle node modules into the main target
    Add example method using the request module
    Add aws-sdk node module
    Change the export strategy of the handler function

--- lambda-template/src/core.cljs
+++ lambda-template/src/core.cljs
@@ -1,8 +1,16 @@
 (ns core
-  (:require [promesa.core :as p]))
+  (:require [promesa.core :as p]
+            ["request" :as request]))
+
+
+(defn http-get-example [url]
+  (p/create (fn [resolve _]
+              (request/get url (fn [_ _ body] (resolve body))))))
 
 (defn handler [event _]
   (println (:body (js->clj event :keywordize-keys true)))
-  (p/promise (clj->js {:statusCode 200 :body (.stringify js/JSON "Hello from Lambda!")})))
+  (-> (http-get-example "http://example.com")
+      (p/then (fn [x] {:body x :statusCode 200}))
+      (p/then clj->js)))
 
-(set! (.-exports js/module) #js {:handler handler})
+(def export (clj->js {:handler handler}))
